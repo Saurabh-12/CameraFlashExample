2716667b039252810dfe3a8d63d073b84ec388e8
Saurabh Sharma <saurabh_sharma947@yahoo.com>
2013-05-16 15:30:23 +0530

    starting with flash

---
+++ CameraFlashExample/src/com/sks/cameraflash/MainActivity.java
@@ -0,0 +1,32 @@
+package com.sks.cameraflash;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			startActivity(new Intent(getBaseContext(), FlashLightActivity.class));
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
